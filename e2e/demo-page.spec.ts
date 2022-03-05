0d89805aa8d9c1f21c7a32dd8d5b8cc7cba6d065
Aditya Agarwal <adityaa803@gmail.com>
2022-03-05 16:10:15 +0530

    second test written

--- e2e/demo-page.spec.ts
+++ e2e/demo-page.spec.ts
@@ -1,10 +1,21 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Test basic interactions on demo page', () => {
-  test('should be able to increment count', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('/demo');
+  });
+
+  test('should be able to increment count @priority=critical', async ({ page }) => {
     const incrementBtnLocator = page.locator('button >> text=Increment Count');
     await incrementBtnLocator.click();
     await expect(page.locator('strong >> text=1')).toBeVisible();
+  });
+
+  test('should be able to toggle mute @priority=normal', async ({ page }) => {
+    const unmuteLocator = page.locator('label >> text=Audible');
+    const muteLocator = page.locator('label >> text=Muted');
+
+    await unmuteLocator.check();
+    await expect(muteLocator).toBeChecked();
   });
 });
